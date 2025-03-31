814002cac24f601bf18f694a871f3a7c688205df
jamesvv27 <jaimesantiagoalatorre1b@gmail.com>
2025-03-31 11:15:06 -0600

    Ejemplos codigo

---
+++ cap-04/src/array_matrix.js
@@ -0,0 +1,4 @@
+let array = [1,2,3];
+let matrix = [array, [4,5,6], [7,8,9]];
+
+console.log(matrix); // [0] => [1,2,3]
---
+++ cap-04/src/object.js
@@ -0,0 +1,6 @@
+let rectangle = {
+	upperLeft: { x: 2, y: 2 },
+	lowerRight: { x: 4, y: 5 }
+};
+
+console.log(rectangle.upperLeft.y) // => 2

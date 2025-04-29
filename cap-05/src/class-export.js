c84926331cd90832630f710621663256c9c83477
jamesvv27 <jaimesantiagoalatorre1b@gmail.com>
2025-04-29 13:55:36 -0600

    Renombrar class-

---
+++ cap-05/src/class-export.js
@@ -0,0 +1,5 @@
+export default class Circle {
+    constructor(radius) { this.r = radius; }
+    area() { return Math.PI * this.r * this.r; }
+    circumference() { return 2 * Math.PI * this.r; }
+}
--- cap-05/src/class.js
+++
@@ -1,5 +0,0 @@
-export default class Circle {
-	constructor(radius) { this.r = radius; }
-	area() { return Math.PI * this.r * this.r; }
-	circumference() { return 2 * Math.PI * this.r; }
-}
